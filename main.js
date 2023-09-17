3230dc8fbe632b2ee46a06c513d443a69a5fbd6b
dwinandar <adedwinandar50@gmail.com>
2023-09-17 11:48:57 +0700

    adding error handling and fix css responsive

--- main.js
+++ main.js
@@ -1,44 +1,58 @@
 const API_URL = "https://api.github.com/users/";
 
-const searchByUsername = async (username) => {
-  const response = await fetch(`${API_URL}${username}`);
-  const data = await response.json();
+// fetch user and generate card user
 
-  if (data.login) {
-    document.querySelector(".result").innerHTML = `
-  <div class="wrapper">
-    <img src="${data.avatar_url}" alt="profile-image" class="profile-img"/>
-    <div class="info">
-      <h2>${data.name}(${data.login})</h2>
-      <h3>${data.bio}</h3>
-      <h4>followers : ${data.followers} | following : ${data.following}</h4>
-      <div class="repos"></div>
+const fetchGithubUser = async (username) => {
+  try {
+    const response = await fetch(`${API_URL}${username}`);
+    const data = await response.json();
+    if (data.login) {
+      document.querySelector(".result").innerHTML = `
+      <img src="${data.avatar_url}" alt="profile-image" class="profile-img"/>
+      <div class="info">
+        <h2>${data.name}(${data.login})</h2>
+        <h3>${data.bio}</h3>
+        <h4>followers : ${data.followers} | following : ${data.following}</h4>
+        <div class="repos"></div>
+      </div>
+    `;
+    } else {
+      document.querySelector(".result").innerHTML = `
+    <div class="wrapper">
+      <img src="" alt="profile-image" class="profile-img"/>
+      <div>
+        <h1>User Not Found</h1>
+      </div>
     </div>
-  </div>
-  `;
-  } else {
-    document.querySelector(".result").innerHTML = `
-  <div class="wrapper">
-    <img src="" alt="profile-image" class="profile-img"/>
-    <div>
-      <h1>User Not Found</h1>
-    </div>
-  </div>
-  `;
+    `;
+    }
+  } catch (err) {
+    alert(err.message);
   }
 };
 
-const fetchRepos = async (username) => {
-  const response = await fetch(`${API_URL}${username}/repos`);
-  const dataRepos = await response.json();
+// fetch repos and insert repos to repos div
 
-  dataRepos.forEach((repos) => {
-    const linkElem = document.createElement("a");
-    linkElem.href = repos.html_url;
-    linkElem.innerText = repos.name;
-    document.querySelector(".repos").appendChild(linkElem);
-  });
+const fetchRepos = async (username) => {
+  let errorNotif = "";
+  try {
+    const response = await fetch(`${API_URL}${username}/repos`);
+    if (!response.ok) errorNotif = Error("Something Went Wrong");
+    const dataRepos = await response.json();
+
+    // make each repos as a link to github repos
+    dataRepos.forEach((repos) => {
+      const linkElem = document.createElement("a");
+      linkElem.href = repos.html_url;
+      linkElem.innerText = repos.name;
+      document.querySelector(".repos").appendChild(linkElem);
+    });
+  } catch (err) {
+    alert(err.message);
+  }
 };
+
+// Handle Submit input
 
 const button = document.querySelector(".button");
 const inputElem = document.querySelector(".username");
@@ -49,7 +63,7 @@ button.addEventListener("click", () => {
     return (document.querySelector(".result").innerHTML =
       "Please Input Username");
   }
-  searchByUsername(inputUser);
+  fetchGithubUser(inputUser);
   fetchRepos(inputUser);
 });
 
